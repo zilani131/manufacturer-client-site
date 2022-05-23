af0dcdecd1ce9406ffa953b7563bb333c516e148
zilani131 <zilani.cuet@gmail.com>
2022-05-23 23:07:27 +0600

    fetching the tool information by id

--- src/Page/Purchase/Purchase.js
+++ src/Page/Purchase/Purchase.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../../Utilities.js/Loading';
 
 const Purchase = () => {
-    const {_id}=useParams()
+    const {_id}=useParams();
     
+    const { isLoading, error, data:tool ,refetch} = useQuery('tool', () =>
+    fetch(`http://localhost:5000/purchase/${_id}`).then(res =>
+      res.json()
+    ))
+ if(isLoading){
+     return <Loading></Loading>
+ }
     return (
         <div>
             purchase page {_id}
+            <h1>{tool?.name}</h1>
         </div>
     );
 };
