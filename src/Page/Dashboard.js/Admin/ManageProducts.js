93e4a16fafc3e36bcaf9835eabf2b7b1820dedd6
zilani131 <zilani.cuet@gmail.com>
2022-05-28 15:29:42 +0600

    dlt prduct modal is added

--- src/Page/Dashboard.js/Admin/ManageProducts.js
+++ src/Page/Dashboard.js/Admin/ManageProducts.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTools from '../../../Hooks/useTools';
 import Loading from '../../../Utilities.js/Loading';
 import ManageProductRow from './ManageProductRow';
+import ProductDlt from './ProductDlt';
 
 const ManageProducts = () => {
     const { isLoading, error, data:tools ,refetch}=useTools();
+    const [dlt,setDlt]=useState(null);
     if(isLoading){
         return <Loading></Loading>
     }
@@ -16,7 +18,7 @@ const ManageProducts = () => {
           {/* <!-- head --> */}
           <thead>
             <tr>
-              <th></th>
+              {/* <th></th> */}
               <th>Product Name</th>
               <th>Image</th>
               <th>Id</th>
@@ -25,17 +27,19 @@ const ManageProducts = () => {
             </tr>
           </thead>
           <tbody>
-            {tools.length && tools.map((tool, index) => (
+            {tools.length && (tools.map((tool, index) => (
               <ManageProductRow
                 key={tool._id}
                 refetch={refetch}
                 index={index}
                 tool={tool}
+                setDlt={setDlt}
               ></ManageProductRow>
-            ))}
+            ))).reverse()}
           </tbody>
         </table>
-      </div>  
+      </div> 
+      {dlt && <ProductDlt refetch={refetch} dlt={dlt} setDlt={setDlt}></ProductDlt>} 
         </div>
     );
 };
---
+++ src/Page/Dashboard.js/Admin/ProductDlt.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const ProductDlt = ({dlt,setDlt,refetch}) => {
+    
+        const {name,_id}=dlt
+    const handleDlt=(event)=>{
+        event.preventDefault();
+        fetch(`http://localhost:5000/product/${_id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data?.deletedCount) {
+              toast.success(`${name} is deleted`);
+            }
+    setDlt(null)
+            refetch();
+         })
+        }
+    return (
+        <div>
+            {/* <!-- The button to open modal --> */}
+
+
+{/* <!-- Put this part before </body> tag --> */}
+<input type="checkbox" id="cancelModal" class="modal-toggle " />
+<div class="modal bg-transparent">
+  <div class="modal-box bg-error">
+    <h3 class="font-bold text-lg">Are you sure you want to delete {name}</h3>
+   
+    <div class="modal-action">
+      <button className='btn btn-success' onClick={handleDlt}>
+          
+          Delete
+      </button>
+      <label for="cancelModal" class="btn">Close</label>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default ProductDlt;
